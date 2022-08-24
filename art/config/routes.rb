6d59bfd95e3ca669b55259f7311b50539d2e33e3
darianchen <darianychen@gmail.com>
2022-08-24 14:55:03 -0400

    set up routes

---
+++ art/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  # resources :users
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  
+  get 'users', to: 'users#index'
+  get 'users', to: 'users#create'
+  get 'users/new', to: 'users#new' 
+  get 'users/:id/edit', to: 'users#edit'
+  get 'users/:id', to: 'users#show'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+  
+
+
+end
