d790140adc4460fc47e76b6f1d5cfa86fe1c6cf5
darianchen <darianychen@gmail.com>
2022-08-25 11:22:27 -0400

    create seed file

--- art/db/seeds.rb
+++ art/db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+  
+#   ActiveRecord::Base.transaction do
+#     puts "Preparing #{Rails.env} environment"
+  
+#     puts 'Destroying tables...'
+#     ArtworkShares.delete_all
+#     Artwork.delete_all
+#     User.delete_all
+    
+  
+#     puts 'Resetting id sequences...'
+#     %w(users videos comments likes).each do |table_name|
+#       ApplicationRecord.connection.reset_pk_sequence!(table_name)
+#     end
+  
+    puts 'Creating seed data...'
+    shawn = User.create!(
+      username: "Mike",
+    )
+  
+    darian = User.create!(
+    username: "Jim",
+    )
+  
+    mona_lisa = Artwork.create!(
+      title: "Mona Lisa",
+      image_url: "www.google.com/monalisa",
+      artist_id: 2
+    )
+
+    boat = Artwork.create!(
+        title: "boat",
+        image_url: "www.google.com/boat",
+        artist_id: 1
+      )
+  
+    
+#     puts "Done with #{Rails.env} environment!"
+#   end
